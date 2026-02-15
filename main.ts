4871a98218f75e5e4f14ca40241d66adfbc208f6
AdrianGorzelewski <gorzelewskiadrian@gmail.com>
2026-02-15 08:51:02 +0100

    Początkowe pliki dla projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showNumber(777)
+basic.showLeds(`
+    # . # . #
+    . # # # .
+    # # # # #
+    . # # # .
+    # . # . #
+    `)
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
